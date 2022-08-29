d72e12a57b18b718ae3cec0c1955d99942577a55
Алексей Никулин <alexeynickulin@yandex.ru>
2022-08-29 21:30:30 +0300

    cjrrected redefinitions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -35,12 +35,9 @@ class IsAuthorOrAdminOrModerator(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated and (
-                request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user
-                or request.method == 'POST'
-            )
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
         )
 
 
